48fcb05a064d8dcf952266c05fc4a328a6e7cf3e
Jose Luis Flores Fuentes <floresfuentes93@gmail.com>
2021-02-13 17:42:59 -0600

    removing unnecesary files

--- src/components/backpanel/AboutMe/UserInfo.js
+++ src/components/backpanel/AboutMe/UserInfo.js
@@ -1,14 +1,34 @@
 import React, { useState } from "react";
 
-import MultipleForm from "../../ui/MultipleForm";
+import DividedInfoFormCore from "../../ui/DividedInfoFormCore";
 
 const UserInfo = () => {
   const [forms, setForms] = useState([]);
 
+  const addNewField = () => {
+    setForms([...forms, { type: "", value: "", id: new Date().getTime() }]);
+  };
+
+  const removeField = (i) => {
+    const arrayFilter = forms.filter(({ id }) => id !== i);
+
+    setForms(arrayFilter);
+  };
+
   return (
     <div className="mt-3">
       <h3>User info</h3>
-      <MultipleForm forms={forms} setForms={setForms} label="Add info" />
+      <span onClick={addNewField} className="itsAPointer">
+        <i className="fas green fa-plus-circle mr-2" />
+        Add info
+      </span>
+      {forms.map(({ type, value, id }) => (
+        <DividedInfoFormCore
+          key={id}
+          FormInitVal={{ type, value }}
+          remove={() => removeField(id)}
+        />
+      ))}
     </div>
   );
 };
